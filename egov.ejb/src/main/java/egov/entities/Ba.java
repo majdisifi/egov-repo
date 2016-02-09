38dacd003c288e816a038c874c4f6584256498e6
aziz <aziz@esprit.tn>
2016-02-09 11:41:33 +0100

    Signed-off-by: aziz <aziz@esprit.tn>

---
+++ egov.ejb/src/main/java/egov/entities/Ba.java
@@ -0,0 +1,21 @@
+package egov.entities;
+
+public class Ba {
+	private String a;
+	private int k;
+	public String getA() {
+		return a;
+	}
+	public void setA(String a) {
+		this.a = a;
+	}
+	public int getK() {
+		return k;
+	}
+	public void setK(int k) {
+		this.k = k;
+	}
+	
+	
+
+}

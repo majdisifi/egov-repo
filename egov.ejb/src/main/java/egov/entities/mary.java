3e588fce799e0c6921bc4834bb39c8cd9ac01316
aziz <aziz@esprit.tn>
2016-02-09 12:06:40 +0100

    Signed-off-by: aziz <aziz@esprit.tn>

--- egov.ejb/src/main/java/egov/entities/mary.java
+++ egov.ejb/src/main/java/egov/entities/mary.java
@@ -1,10 +1,38 @@
 package egov.entities;
 
 public class mary {
-	
 
-	private int pi;
-	private int pio;
-	private int pdf;
+	private String mo;
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+	
 }
 

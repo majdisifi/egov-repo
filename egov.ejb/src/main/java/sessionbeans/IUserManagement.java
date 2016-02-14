b173527fd18862461d6c57bbc4dc227348def6f3
aziz <aziz@esprit.tn>
2016-02-14 21:17:04 +0100

    Signed-off-by: aziz <aziz@esprit.tn>

---
+++ egov.ejb/src/main/java/sessionbeans/IUserManagement.java
@@ -0,0 +1,8 @@
+package sessionbeans;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface IUserManagement {
+
+}
--- egov.ejb/src/main/java/sessionbeans/UserManagement.java
+++ egov.ejb/src/main/java/sessionbeans/UserManagement.java
@@ -1,5 +1,22 @@
 package sessionbeans;
 
-public class UserManagement {
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import egov.entities.User;
 
+
+@Stateless
+public class UserManagement implements IUserManagement {
+	@PersistenceContext
+	EntityManager Us ;
+	
+	public void  addEmploye(User u) {
+		Us.persist(u);
+	}
+	
+	public User findUserById(int id) {
+		return Us.find(User.class,id);
+
+	}
 }

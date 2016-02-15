0d8f99e7196ac1b00f2ad05991123be0e5d938fc
aziz <aziz@esprit.tn>
2016-02-15 19:25:41 +0100

    Signed-off-by: aziz <aziz@esprit.tn>

---
+++ egov.client/src/main/java/projet/client/hhhh.java
@@ -0,0 +1,51 @@
+package projet.client;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class hhhh extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					hhhh frame = new hhhh();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public hhhh() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		textField_1 = new JTextField();
+		contentPane.add(textField_1, BorderLayout.NORTH);
+		textField_1.setColumns(10);
+	}
+
+}

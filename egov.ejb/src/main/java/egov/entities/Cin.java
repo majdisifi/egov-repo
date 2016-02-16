784ac88ccd42a697b1789a4660d10c0f888fbc45
HP <HP@192.168.40.1>
2016-02-16 09:46:32 +0100

    Signed-off-by: HP <HP@192.168.40.1>

--- egov.ejb/src/main/java/egov/entities/Cin.java
+++ egov.ejb/src/main/java/egov/entities/Cin.java
@@ -9,36 +9,40 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+
 @Entity
 
-public class Cin implements Serializable{
-	   @Id
-	   @GeneratedValue(strategy=GenerationType.AUTO)
-	   private int cin;
-	   private Date deliveryDate;
-	   @OneToOne
-		 private User user;
+public class Cin implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int cin;
+	
+	private Date deliveryDate;
+	@OneToOne
+	private User user;
+
 	public int getCin() {
 		return cin;
 	}
+
 	public Date getDeliveryDate() {
 		return deliveryDate;
 	}
-	
+
 	public void setCin(int cin) {
 		this.cin = cin;
 	}
+
 	public void setDeliveryDate(Date deliveryDate) {
 		this.deliveryDate = deliveryDate;
 	}
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	   
-	   
 
 }

6d2eed608ee256d6dfad768c6e1b2110e854e205
mariem <mariem@mariem-PC>
2016-02-16 09:46:41 +0100

    Signed-off-by: mariem <mariem@mariem-PC>

--- egov.client/src/main/java/projet/client/Test_GestionEgov.java
+++ egov.client/src/main/java/projet/client/Test_GestionEgov.java
@@ -9,8 +9,9 @@ import javax.naming.NamingException;
 
 import org.junit.Test;
 
+import egov.entities.GreyCard;
 import egov.entities.User;
-
+import sessionbeans.IGreyCardManagementRemote;
 import sessionbeans.IUserManagementRemote;
 
 public class Test_GestionEgov {
@@ -58,6 +59,31 @@ public class Test_GestionEgov {
 				System.out.println(users1.getFirstName());
 
 			}
+
+		} catch (NamingException e) {
+
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	@Test
+	public void test1() {
+		Context context;
+
+		try {
+			context = new InitialContext();
+
+			IGreyCardManagementRemote ge = (IGreyCardManagementRemote) context
+					.lookup("egov.ejb/GreyCardManagement!sessionbeans.IGreyCardManagementRemote");
+
+			GreyCard GreyCard = new GreyCard();
+			GreyCard.setIdGreyCard(1);
+			GreyCard.setCategory("mmm");
+			GreyCard .setConstructor("jjjjj");
+		    GreyCard.setCar(null);
+		    GreyCard.setNumImmatriculation(5666);
+			ge.addGreyCard(GreyCard);
 
 		} catch (NamingException e) {
 

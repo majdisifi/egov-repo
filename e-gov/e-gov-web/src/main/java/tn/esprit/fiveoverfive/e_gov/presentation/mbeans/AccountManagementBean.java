01da5eda1998bc30151349a0e3f36f0a40118481
aziz <aziz@esprit.tn>
2016-04-12 12:27:41 +0200

    Signed-off-by: aziz <aziz@esprit.tn>

--- e-gov/e-gov-web/src/main/java/tn/esprit/fiveoverfive/e_gov/presentation/mbeans/AccountManagementBean.java
+++ e-gov/e-gov-web/src/main/java/tn/esprit/fiveoverfive/e_gov/presentation/mbeans/AccountManagementBean.java
@@ -1,5 +1,8 @@
 package tn.esprit.fiveoverfive.e_gov.presentation.mbeans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -14,14 +17,10 @@ public class AccountManagementBean {
 	@EJB
 	private IAccountManagementLocal iAccountManagementLocal;
 
-	// models
-	private Account account = new Account();
+	private List<Account> accounts = new ArrayList<>();
+	private Account accountSelected = new Account();
 
-	// methodes
-	public String doAddAccount() {
-		iAccountManagementLocal.addAccount(account);
-		return "";
-	}
+	private Account account = new Account();
 
 	public Account getAccount() {
 		return account;
@@ -29,6 +28,44 @@ public class AccountManagementBean {
 
 	public void setAccount(Account account) {
 		this.account = account;
+	}
+
+	public Account getAccountSelected() {
+		return accountSelected;
+	}
+
+	public void setAccountSelected(Account accountSelected) {
+		this.accountSelected = accountSelected;
+	}
+
+	// methodes
+	public String doAddAccount() {
+		iAccountManagementLocal.addAccount(account);
+		return "/pages/accountManagement/listAccounts?faces-redirect=true";
+	}
+
+	public String doDeleteAccount(Account accountSelected) {
+		iAccountManagementLocal.removeAccount(accountSelected);
+		return "/pages/accountManagement/listAccounts?faces-redirect=true";
+
+	}
+
+	public String doUpdateAccount() {
+		iAccountManagementLocal.updateAccount(accountSelected);
+		return "/pages/accountManagement/listAccounts?faces-redirect=true";
+	}
+
+	public void doSelectAccount(Account c) {
+		accountSelected = c;
+	}
+
+	public List<Account> getAccounts() {
+		accounts = iAccountManagementLocal.findAll();
+		return accounts;
+	}
+
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
 	}
 
 }

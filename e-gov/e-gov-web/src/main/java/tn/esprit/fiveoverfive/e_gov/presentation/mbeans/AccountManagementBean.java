2d04fb56c7636110f080a5c4b51f67bed3411bca
aziz <aziz@esprit.tn>
2016-04-14 02:53:39 +0200

    Signed-off-by: aziz <aziz@esprit.tn>

--- e-gov/e-gov-web/src/main/java/tn/esprit/fiveoverfive/e_gov/presentation/mbeans/AccountManagementBean.java
+++ e-gov/e-gov-web/src/main/java/tn/esprit/fiveoverfive/e_gov/presentation/mbeans/AccountManagementBean.java
@@ -3,69 +3,66 @@ package tn.esprit.fiveoverfive.e_gov.presentation.mbeans;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 import egov.entities.Account;
+import egov.entities.Car;
+import egov.entities.User;
 import sessionbeans.IAccountManagementLocal;
 
 @ManagedBean
-@SessionScoped
+@ViewScoped
 public class AccountManagementBean {
-	// injection
-	@EJB
-	private IAccountManagementLocal iAccountManagementLocal;
+private IAccountManagementLocal iAccountManagementLocal;
+private List<Account> accounts = new ArrayList<>();
+private Account account = new Account();
+private Account accountSelected = new Account();
 
-	private List<Account> accounts = new ArrayList<>();
-	private Account accountSelected = new Account();
+public IAccountManagementLocal getiAccountManagementLocal() {
+	return iAccountManagementLocal;
+}
+public void setiAccountManagementLocal(IAccountManagementLocal iAccountManagementLocal) {
+	this.iAccountManagementLocal = iAccountManagementLocal;
+}
+public List<Account> getAccounts() {
 
-	private Account account = new Account();
+	accounts = iAccountManagementLocal.findAll();
+	return accounts;
+}
+public void setAccounts(List<Account> accounts) {
+	this.accounts = accounts;
+}
+public Account getAccountSelected() {
+	return accountSelected;
+}
+public void setAccountSelected(Account accountSelected) {
+	this.accountSelected = accountSelected;
+}
+public Account getAccount() {
+	return account;
+}
+public void setAccount(Account account) {
+	this.account = account;
+}
 
-	public Account getAccount() {
-		return account;
-	}
 
-	public void setAccount(Account account) {
-		this.account = account;
-	}
+public String doAddAccount() {
+	iAccountManagementLocal.addAccount(account);
+	return "/pages/accountManagement/listAccounts?faces-redirect=true";
+}
 
-	public Account getAccountSelected() {
-		return accountSelected;
-	}
+public String doDeleteAccount(Account accountSelected) {
+	iAccountManagementLocal.removeAccount(accountSelected);
+	return "/pages/accountManagement/listAccounts?faces-redirect=true";
+}
 
-	public void setAccountSelected(Account accountSelected) {
-		this.accountSelected = accountSelected;
-	}
+public String doUpdateAccount() {
+	iAccountManagementLocal.updateAccount(accountSelected);
+	return "/pages/accountManagement/listAccounts?faces-redirect=true";
+}
 
-	// methodes
-	public String doAddAccount() {
-		iAccountManagementLocal.addAccount(account);
-		return "/pages/accountManagement/listAccounts?faces-redirect=true";
-	}
-
-	public String doDeleteAccount(Account accountSelected) {
-		iAccountManagementLocal.removeAccount(accountSelected);
-		return "/pages/accountManagement/listAccounts?faces-redirect=true";
-
-	}
-
-	public String doUpdateAccount() {
-		iAccountManagementLocal.updateAccount(accountSelected);
-		return "/pages/accountManagement/listAccounts?faces-redirect=true";
-	}
-
-	public void doSelectAccount(Account c) {
-		accountSelected = c;
-	}
-
-	public List<Account> getAccounts() {
-		accounts = iAccountManagementLocal.findAll();
-		return accounts;
-	}
-
-	public void setAccounts(List<Account> accounts) {
-		this.accounts = accounts;
-	}
-
+public void doSelectAccount(Account u) {
+	accountSelected = u;
+}
 }
